b1ce03f39011cd4b6b06a2414c225db4b92d4bd5
42bahn <57256332+42bahn@users.noreply.github.com>
2021-05-25 16:38:44 +0900

    Create ccf.php

---
+++ Day01/ex04/ccf.php
@@ -0,0 +1,35 @@
+<?php
+function capital_city_from($name)
+{
+	$states = [
+    'Oregon' => 'OR',
+    'Alabama' => 'AL',
+    'New Jersey' => 'NJ',
+    'Colorado' => 'CO',
+	];
+	$capitals = [
+		'OR' => 'Salem',
+		'AL' => 'Montgomery',
+		'NJ' => 'trenton',
+		'KS' => 'Topeka',
+	];
+	
+	foreach ($states as $key1 => $value1){
+		if ($name == $key1)	{
+			foreach ($capitals as $key2 => $value2) {
+				if ($value1 == $key2) {
+					echo $value2."\n";
+				} else {
+					echo "Unknown";
+				}
+			}
+		} else {
+			echo "Unknown";
+		}
+	}
+}
+
+capital_city_from("Alabama");
+capital_city_from("Oregan");
+
+?>
